d5ad7aac4b10f892bbcf4fd24c88b1306b469b1d
CherrySalute <cherrysalute0628@gmail.com>
2024-04-12 13:49:44 +0900

    HomeView import

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script setup>
-
+import HeaderSection from "@/components/HeaderSection.vue";
+import FooterSection from "@/components/FooterSection.vue";
+import IntroSection from "@/components/IntroSection.vue";
+import CarrerSection from "@/components/CarrerSection.vue";
+import PortfolioSection from "@/components/PortfolioSection.vue";
+import ContactSection from "@/components/ContactSection.vue";
 </script>
 
 <template>
+    <HeaderSection />
     <main id="main" role="main">
-        
+        <IntroSection />
+        <CarrerSection />
+        <PortfolioSection />
+        <ContactSection />
     </main>
+    <FooterSection />
 </template>
